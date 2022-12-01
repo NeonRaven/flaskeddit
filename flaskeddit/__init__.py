8825c9b803f0eb57477d5914d0c7c728a91d3e94
hayden <z5056635@ad.unsw.edu.au>
2022-12-02 08:22:45 +1100

    Modified requirements.txt to work with python 3.10
    Added DB creation
    
    Added run script

--- flaskeddit/__init__.py
+++ flaskeddit/__init__.py
@@ -19,6 +19,11 @@ def create_app(config=Config):
     app.config.from_object(config)
 
     db.init_app(app)
+
+    from flaskeddit import models
+    with app.app_context():
+        db.create_all()
+
     login_manager.init_app(app)
 
     from flaskeddit.auth import auth_blueprint
--- flaskeddit/config.py
+++ flaskeddit/config.py
@@ -5,5 +5,5 @@ class Config:
     """Configuration settings for the Flaskeddit Flask app."""
 
     SECRET_KEY = os.environ.get("SECRET_KEY", "placeholder-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///app.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "mysql+pymysql://root:password@127.0.0.1:3306/flaskeddit")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
